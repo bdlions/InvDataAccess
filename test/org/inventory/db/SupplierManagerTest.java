6324fd4cf6e3c4ca3c5cdb3c7e9a17337ba991f8
rashidacse <rashida57pust@gmail.com>
2016-08-23 15:40:26 +0600

    supplier manager test

---
+++ test/org/inventory/db/SupplierManagerTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inventory.db;
+
+import com.inventory.bean.ProfileInfo;
+import com.inventory.bean.SupplierInfo;
+import com.inventory.db.manager.SupplierManager;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author sampanit
+ */
+public class SupplierManagerTest {
+   final static SupplierManager supplierManager = new SupplierManager(); 
+    public SupplierManagerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+//    @Test
+
+    public void createSupplierTest() {
+        ProfileInfo userInfo = new ProfileInfo();
+        userInfo.setFirstName("Customer1");
+        userInfo.setLastName("ln1");
+        userInfo.setEmail("email1");
+        userInfo.setPhone("phone1");
+        userInfo.setFax("fax1");
+        SupplierInfo supplierInfo = new SupplierInfo();
+        supplierInfo.setProfileInfo(userInfo);
+        supplierInfo.setRemarks("remarks2");
+        supplierManager.createSupplier(supplierInfo);
+    }
+
+//    @Test
+    public void getAllSuppliersTest() {
+        List<SupplierInfo> supllierList = supplierManager.getAllSuppliers();
+        System.out.println(supllierList);
+    }
+  
+//    @Test
+    public void getSupplierTest() {
+        SupplierInfo supllierInfo = supplierManager.getSuplierInfo(1);
+        System.out.println(supllierInfo.getProfileInfo().getFirstName());
+    }
+}
